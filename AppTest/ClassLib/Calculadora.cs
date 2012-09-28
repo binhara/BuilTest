a276fd04eeec71d61d9016585a2e04d2a7fa1257
binhara <binhara@gmail.com>
2012-09-28 17:06:22 -0300

    primeira versao

---
+++ AppTest/ClassLib/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLib
+{
+    public class Calculadora
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Soma()
+        {
+            return X + Y;
+        }
+
+        public int Multiplica()
+        {
+            return X * Y;
+        }
+    }
+}
---
+++ LibTest/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using ClassLib;
+
+namespace LibTest
+{
+   
+  [TestFixture]
+  public class Test
+  {
+    [Test]
+    public void Soma()
+    {
+        Calculadora c = new Calculadora();
+        c.X = 2;
+        c.Y = 4;  
+        	  
+      Assert.AreEqual(6, c.Soma());
+
+    }
+
+    [Test]
+    public void Multiplica()
+    {
+        Calculadora c = new Calculadora();
+        c.X = 2;
+        c.Y = 4;
+
+        Assert.AreEqual(8, c.Multiplica());
+
+    }
+  }
+}
